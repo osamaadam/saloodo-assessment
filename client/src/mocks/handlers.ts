3a2ae61ce28ca8993262a0049739cc9ce878a748
Osama Adam <osamaadamme@gmail.com>
2023-04-27 13:31:08 +0200

    adding `msw` to the project

---
+++ client/src/mocks/handlers.ts
@@ -0,0 +1,19 @@
+import { rest } from "msw";
+
+export const dummyUser = {
+  user: {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "test@test.com",
+    role: "CLIENT",
+  },
+  accessToken: "dummyAccessToken",
+  refreshToken: "dummyRefreshToken",
+};
+
+const loginHandler = rest.post("/auth/login", (_, res, ctx) => {
+  return res(ctx.json(dummyUser));
+});
+
+export const handlers = [loginHandler];
